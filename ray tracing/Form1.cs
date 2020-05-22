fe8da3733a49db36b9b3cc8519e127bd28ac1fab
DenisOgnev <denisognev@icloud.com>
2020-05-22 13:57:47 +0300

    first commit

---
+++ ray tracing/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace ray_tracing
+{
+    public partial class Form1 : Form
+    {
+        bool loaded = false;
+        View view;
+
+        public Form1()
+        {
+            InitializeComponent();
+            view = new View();
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!loaded) return;
+            view.Update();
+            glControl1.SwapBuffers();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            loaded = true;
+            view.Setup(glControl1.Width, glControl1.Height);
+        }
+    }
+}
---
+++ ray tracing/View.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace ray_tracing
+{
+    class View
+    {
+        int BasicProgramID;
+        int BasicVertexShader;
+        int BasicFragmentShader;
+        int vbo_position;
+        Vector3[] vertdata;
+        public int attribute_vpos;
+        int uniform_pos;
+        Vector3 campos;
+        int uniform_aspect;
+        double aspect;
+
+        public View()
+        {
+            vertdata = new Vector3[]
+            {
+                new Vector3(-1f, -1f, 0f),
+                new Vector3(1f, -1f, 0f),
+                new Vector3(1f, 1f, 0f),
+                new Vector3(-1f, 1f, 0f)
+            };
+        }
+
+        public void Setup(int width, int height)
+        {
+            GL.ClearColor(System.Drawing.Color.DarkGray);
+            GL.ShadeModel(ShadingModel.Smooth);
+
+            InitShaders();
+            InitBuffer();
+        }
+
+        public void Update()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Enable(EnableCap.DepthTest);
+
+            GL.EnableVertexAttribArray(attribute_vpos);
+            GL.DrawArrays(PrimitiveType.Quads, 0, 4);
+            GL.DisableVertexAttribArray(attribute_vpos);
+        }
+
+        void LoadShader(string filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
+
+        private void InitShaders()
+        {
+            BasicProgramID = GL.CreateProgram();
+            LoadShader("..\\..\\raytracing.vert", ShaderType.VertexShader, BasicProgramID, out BasicVertexShader);
+            LoadShader("..\\..\\raytracing.frag", ShaderType.FragmentShader, BasicProgramID, out BasicFragmentShader);
+            GL.LinkProgram(BasicProgramID);
+            int status = 0;
+            GL.GetProgram(BasicProgramID, GetProgramParameterName.LinkStatus, out status);
+            Console.WriteLine(GL.GetProgramInfoLog(BasicProgramID));
+        }
+
+        private void InitBuffer()
+        {
+            GL.GenBuffers(1, out vbo_position);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo_position);
+
+            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(vertdata.Length * Vector3.SizeInBytes),
+                                                                        vertdata, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(attribute_vpos, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.Uniform3(uniform_pos, campos);
+            GL.Uniform1(uniform_aspect, aspect);
+            GL.UseProgram(BasicProgramID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        }
+    }
+}
